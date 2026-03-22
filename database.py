988904e4cfd157e30395a9cef75b506b7e65cb95
Chandani <chandniaditya65@gmail.com>
2026-03-22 10:53:57 +0530

    Add postgres

--- database.py
+++ database.py
@@ -1,17 +1,14 @@
+import os
 from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker, declarative_base
 
-DATABASE_URL = "sqlite:///./exampressure.db"
+DATABASE_URL = os.getenv("DATABASE_URL", "sqlite:///./exampressure.db")
 
-engine = create_engine(
-    DATABASE_URL,
-    connect_args={"check_same_thread": False}
-)
+if DATABASE_URL.startswith("postgres://"):
+    DATABASE_URL = DATABASE_URL.replace("postgres://", "postgresql://", 1)
 
-SessionLocal = sessionmaker(
-    autocommit=False,
-    autoflush=False,
-    bind=engine
-)
+engine = create_engine(DATABASE_URL)
+
+SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
 
 Base = declarative_base()
