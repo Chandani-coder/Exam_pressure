a330ebdfe9656e307b404f1a05aa7192eba90379
Chandani <chandniaditya65@gmail.com>
2026-04-28 12:26:10 +0530

    fix:ssl connection issue

--- database.py
+++ database.py
@@ -20,7 +20,10 @@ if DATABASE_URL.startswith("postgres://"):
 engine = create_engine(
     DATABASE_URL,
     pool_pre_ping=True,
-    connect_args={"sslmode": "require"}
+    connect_args={
+        "sslmode": "require",
+        "connect_timeout": 10
+    }
 )
 
 # Session
